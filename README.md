7f78b37b05635327eadc0eb81a3520f506f93e1e
lachlan greenbank <lachlan.greenbank@gmail.com>
2019-07-03 14:02:53 +1000

    upadted readme

--- README.md
+++ README.md
@@ -1,14 +1,50 @@
-# vue-node-example
+### Smartpress Node.js / VUE.js example application
+
+## 🧐 About
+
+<a href="https://app.smart.press">Smartpress platform</a>
+<a href="https://www.smart.press">Smartpress website</a>
+
+## Getting Started
+
+To run a basic example, run the following commands:
 
 ## Project setup
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Runs frontend
 ```
 npm run serve
 ```
+
+## Runs backend
+```
+node node/index.js
+```
+
+## Usage
+
+To connect smartpress to your server, see the following example in node/index.js
+
+```js
+const Smartpress = require('smartpress')
+const spConfig = require('../SmartpressAPI/smartpressConfig.js')
+
+const smartpress = new Smartpress(spConfig)
+
+app.post('/createAccount', async function (req, res) {
+	if (typeof req.body.data.user_id == 'string') {
+		let data = await smartpress.createAccount({user_id: req.body.data.user_id})
+	    res.send({success: true, data: data})
+	} else {
+		res.send({success: false, data: {message:"Missing string user_id"}})
+	}
+	return;
+})
+```
+
 
 ### Compiles and minifies for production
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,26 @@
 <template>
   <span>
     <div class="home">
-      <img alt="Smartpress logo" src="../assets/logo.png">
+      <img height="100px" alt="Smartpress logo" src="../assets/logo.png">
     </div>
 
     <div>  
+      <h4>Create account</h4>
+      <input style="margin:10px;" placeholder="create user_id" v-model="create_user_id" />
+      <button @click="createAccount()">Create</button> <br/><br/>
+      <span v-if="create.data" :key="create.data.data.user_id">Create: {{create.data.data}}</span><br/><br/><br/>
+
+      <h4>Send asset</h4>
       <input style="margin:10px;" placeholder="sender" v-model="sender" />
       <input style="margin:10px;" placeholder="receiver" v-model="receiver" />
       <input style="margin:10px;" placeholder="amount" type="number" v-model="amount" />
       <button @click="sendAsset()">Send</button> <br/><br/>
+      <span v-if="send.data" :key="send.data.data.user_id">Send: {{send.data.data}}</span><br/><br/><br/>
 
+      <h4>Get account balance</h4>
       <input style="margin:10px;" placeholder="Get balance of user_id" v-model="balance_user_id" />
       <button @click="accountBalance()">Balance</button> <br/><br/>
-
-      <input style="margin:10px;" placeholder="create user_id" v-model="create_user_id" />
-      <button @click="createAccount()">Create</button> <br/><br/>
-
-      <span v-if="balance.data" :key="balance.data.data.user_id">Balance: {{balance.data.data}}</span><br/><br/><br/><br/>
-      <span v-if="create.data" :key="create.data.data.user_id">Create: {{create.data.data}}</span><br/><br/><br/><br/>
-      <span v-if="send.data" :key="send.data.data.user_id">Send: {{send.data.data}}</span><br/><br/><br/><br/>
+      <span v-if="balance.data" :key="balance.data.data.user_id">Balance: {{balance.data.data}}</span><br/><br/><br/>
     </div>
   </span>
 </template>
